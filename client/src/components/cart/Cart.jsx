fed01e8c9b0b0221a66175128e488cc2c1603402
Astha <iaanegi@gmail.com>
2023-07-25 12:17:22 +0530

    fetch the cart items from redux store to the Cart component and divided the cart component into left and right part

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,9 +1,35 @@
+import {Typography,Grid,Box} from "@mui/material";
+import {useSelector} from "react-redux";
 
+// components 
+import CartItem from "./CartItem";
+import TotalView from "./TotalView";
 
 const Cart=()=>{
+
+    const {cartItems} = useSelector(state=>state.cart);
+
     return(
         <>
-            Hello from Cart
+            {
+                cartItems.length?
+                   <Grid container >
+                      <Grid item lg={9} md={9} sm={12} xs={12}>
+                        <Box>
+                            <Typography>My Cart ({cartItems.length})</Typography>
+                        </Box>
+                        {
+                            cartItems.map(item=>(
+                               <CartItem/>
+                            ))
+                        }
+                      </Grid>
+                      <Grid item lg={3} md={3} sm={12} xs={12}>
+                        <TotalView/>
+                      </Grid>
+                   </Grid>
+                   :<div>Empty</div>
+            }
         </>
     );
 }
---
+++ client/src/components/cart/TotalView.jsx
@@ -0,0 +1,8 @@
+
+const TotalView=()=>{
+    return(
+        <div>right side</div>
+    );
+}
+
+export default TotalView;
