2504f70f3287953553f0f8ec5e52a2dc715a857f
Astha <iaanegi@gmail.com>
2023-07-25 21:36:48 +0530

    fixed position of Container in Cart.jsx and added responsiveness to the Cart.jsx

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -6,9 +6,13 @@ import CartItem from "./CartItem";
 import TotalView from "./TotalView";
 import EmptyCart from "./EmptyCart";
 
-const Container =styled(Grid)`
-  padding:30px 135px;
-`;
+const Container =styled(Grid)(({theme})=>({
+  padding:"30px 135px",
+  [theme.breakpoints.down("md")]:{
+    padding:"15px 0"
+  }
+}))
+  
 
 const Header=styled(Box)`
   padding:15px 24px;
@@ -32,6 +36,15 @@ const StyledButton=styled(Button)`
   border-radius:2px;
 `;
 
+const LeftComponent=styled(Grid)(({theme})=>({
+
+  paddingRight:15,
+  [theme.breakpoints.down("md")]:{
+    marginBottom:15
+  }
+}))
+  
+
 const Cart=()=>{
 
     const {cartItems} = useSelector(state=>state.cart);
@@ -40,8 +53,8 @@ const Cart=()=>{
         <>
             {
                 cartItems.length?
-                   <Grid container >
-                      <Container item lg={9} md={9} sm={12} xs={12}>
+                   <Container container >
+                      <LeftComponent item lg={9} md={9} sm={12} xs={12}>
                         <Header>
                             <Typography>My Cart ({cartItems.length})</Typography>
                         </Header>
@@ -53,11 +66,11 @@ const Cart=()=>{
                         <ButtonWrapper>
                             <StyledButton>Place Order</StyledButton>
                         </ButtonWrapper>
-                      </Container>
+                      </LeftComponent>
                       <Grid item lg={3} md={3} sm={12} xs={12}>
                         <TotalView cartItems={cartItems}/>
                       </Grid>
-                   </Grid>
+                   </Container>
                    :<EmptyCart/>
             }
         </>
